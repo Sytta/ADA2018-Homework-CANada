1c09a3d3726e2d915dd0ce9fc99c9e54e08b8cab
YawenHou <yawen.hou@epfl.ch>
2018-10-17 01:26:14 +0200

    Hw2 Q2 yw

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,4 +1,4 @@
-git remote add upstream https://github.com/Sytta/ADA2018-Homework-CANada.git
+#git remote add upstream https://github.com/epfl-ada-2018/ADA2018-Homework.git
 git fetch upstream
 git checkout master
 git merge upstream/master
